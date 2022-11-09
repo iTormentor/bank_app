b135f09d9ec8e823103d6a6d8a7189b2415627b0
Mantas <mantasx5@gmail.com>
2022-11-09 13:03:38 +0100

    Implemented sign in page

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Bank App'),
+        elevation: 10.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Image(image: AssetImage("assets/images/BankLogo.jpg")),
+            const Text(
+              "Sign In",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            ElevatedButton(
+              child: const Text("Sign in with email"),
+              onPressed: () {},
+            ),
+          ],
+      ),
+    );
+  }
+}
