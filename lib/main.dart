98e2903e1db411a9573c0c209a4df43afda41a6c
iTormentor <toranders98@gmail.com>
2022-11-23 11:50:53 +0100

    Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:bank_app/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bank_app/app/sign_in/sign_in_page.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
